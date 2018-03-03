288544c0ab3fc5a9f519999aa0f1e6661336845a
Johan Odijk <dutchderpyguy@gmail.com>
2018-03-03 16:02:40 +0100

    added post request via postURL, not fully tested!

--- joo.js
+++ joo.js
@@ -15,6 +15,27 @@ jooJS = {
     xhttp.open("GET", url, true);
     xhttp.send();
   },
+  postURL: function(path, params) { //gotten from https://stackoverflow.com/a/133997., slightly modified to make it only accept post.
+    // The rest of this code assumes you are not using a library.
+    // It can be made less wordy if you use one.
+    var form = document.createElement("form");
+    form.setAttribute("method", "post");
+    form.setAttribute("action", path);
+
+    for(var key in params) {
+        if(params.hasOwnProperty(key)) {
+            var hiddenField = document.createElement("input");
+            hiddenField.setAttribute("type", "hidden");
+            hiddenField.setAttribute("name", key);
+            hiddenField.setAttribute("value", params[key]);
+
+            form.appendChild(hiddenField);
+        }
+    }
+
+    document.body.appendChild(form);
+    form.submit();
+  },
   shuffleArray: function(array) { //shuffles an array using the Fisher-Yates algorithm SRC: https://stackoverflow.com/a/2450976 & https://bost.ocks.org/mike/shuffle/
       if (Array.isArray(array)){
       var currentIndex = array.length, temporaryValue, randomIndex;
@@ -45,7 +66,7 @@ jooJS = {
     text = this.defaultParam(text,null);
     div = this.defaultParam(div,false);
     if (div) {
-      el = this.makeEl("div",text); 
+      el = this.makeEl("div",text);
     } else {
       el = this.makeEl("button",text);
     }
